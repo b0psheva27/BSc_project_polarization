c728328c662e0339032b34dab1c027fdf45c8cee
Josefine Nyeng <finenyeng@gmail.com>
2025-03-17 14:10:10 +0100

    topic combinations EDA
    Co-Authored-By: b0psheva27 <129192578+b0psheva27@users.noreply.github.com>

---
+++ bertopic_modeling_oldmodel.py
@@ -0,0 +1,53 @@
+# Imports
+from bertopic import BERTopic
+import pandas as pd
+import numpy as np
+import nltk 
+import matplotlib.pyplot as plt
+
+# getting weekly data 
+messages_df = pd.read_csv("reddit_22_51/messages.csv", sep="\t")
+# convert to list of strings (input needed by bertopic model)
+messages_list = messages_df["text"].astype(str).tolist()
+
+topic_model_fitted = BERTopic.load("models/bert_topic_model")
+
+print("Model loaded")
+
+# get info on topics (names, important words, representative reddit message/document) !!!before reclustering
+topic_info = topic_model_fitted.get_topic_info() #topics with their indeces and repr words
+topic_info.to_csv("output/weekly_topics_oldmodel_2.csv", sep="\t", index=False)
+
+weekly_topics = topic_model_fitted.topics_  #topics per message
+# Store topics in the original DataFrame
+messages_df['topic'] = weekly_topics
+
+# Create a mapping of topic ID to topic name
+topic_id_to_name = topic_info.set_index("Topic")["Name"].to_dict()
+
+# Map topic IDs to topic names in the DataFrame
+messages_df["topic_name"] = messages_df["topic"].map(topic_id_to_name)
+messages_df.to_csv("output/weekly_data_oldmodel_2.csv", sep=",")
+
+print("############## Transforming new data ################")
+monthly_messages_df = pd.read_csv("month_data/22_12.csv", sep="\t")
+
+# Transform the text column (no need to refit)
+topics, probs = topic_model_fitted.transform(monthly_messages_df['text'].astype(str).tolist())
+
+# Store topics in the original DataFrame
+monthly_messages_df['topic'] = topics
+
+# Create a mapping of topic ID to topic name
+topic_id_to_name = topic_info.set_index("Topic")["Name"].to_dict()
+
+# Map topic IDs to topic names in the DataFrame
+monthly_messages_df["topic_name"] = monthly_messages_df["topic"].map(topic_id_to_name)
+
+print("Saving the topics per message for December 2022...")
+monthly_messages_df.to_csv("output/monthly_data_oldmodel_2.csv", sep=",")
+# We don't save the topics with their ids and repr words/docs for the monthly data, since they are supposed to be the same as the weekly ones 
+# (the model is fitted on the weekly data only)
+
+print("Done!")
+
