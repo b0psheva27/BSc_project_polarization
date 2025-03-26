3d98d778b59767495c8381a4764f0c896301c091
Josefine Nyeng <finenyeng@gmail.com>
2025-03-26 10:30:31 +0100

    filtered network to get largest connected component of relevant users
    Co-Authored-By: b0psheva27 <129192578+b0psheva27@users.noreply.github.com>

---
+++ polarization_function.py
@@ -0,0 +1,47 @@
+import networkx as nx
+import numpy as np
+from scipy.special import binom
+from scipy.sparse import csgraph
+
+# Function to calculate the pseudoinverse of the Laplacian of the network
+def _ge_Q(network):
+    A = nx.adjacency_matrix(network).todense().astype(float)
+    return np.linalg.pinv(csgraph.laplacian(np.matrix(A), normed=False))
+
+def ge(src, trg, network, Q=None):
+    """Calculate GE for network.
+
+    Parameters:
+    ----------
+    srg: vector specifying node polarities
+    trg: vector specifying node polarities
+    network: networkx graph
+    Q: pseudoinverse of Laplacian of the network
+    """
+    if nx.number_connected_components(network) > 1:
+        raise ValueError("""Node vector distance is only valid if calculated on a network with a single connected component.
+                       The network passed has more than one.""")
+    src = np.array([src[n] if n in src else 0. for n in network.nodes()])
+    trg = np.array([trg[n] if n in trg else 0. for n in network.nodes()])
+    diff = src - trg
+    if Q is None:
+        Q = _ge_Q(network)
+
+    ge_dist = diff.T.dot(np.array(Q).dot(diff))
+
+    if ge_dist < 0:
+        ge_dist = 0
+
+    return np.sqrt(ge_dist)
+
+# This function reads the edgelist and the opinion value
+# It returns the delta polarization score
+def calc_pol(path):
+   G = nx.read_edgelist(f"{path}_edgelist.csv", delimiter = ',')
+   o = {}
+   with open(f"{path}_user_scores.csv", 'r') as f:
+      for line in f:
+         fields = line.strip().split(',')
+         if fields[0] in G.nodes:
+            o[fields[0]] = float(fields[1])
+   return ge(o, {}, G)
