30ec6b1d944399165662fec6bf57643e015014aa
Josefine Nyeng <finenyeng@gmail.com>
2025-03-26 11:43:12 +0100

    computed polarization score on dummy dataset
    Co-Authored-By: b0psheva27 <129192578+b0psheva27@users.noreply.github.com>

--- polarization_function.py
+++ polarization_function.py
@@ -2,6 +2,7 @@ import networkx as nx
 import numpy as np
 from scipy.special import binom
 from scipy.sparse import csgraph
+import pandas as pd 
 
 # Function to calculate the pseudoinverse of the Laplacian of the network
 def _ge_Q(network):
@@ -37,11 +38,45 @@ def ge(src, trg, network, Q=None):
 # This function reads the edgelist and the opinion value
 # It returns the delta polarization score
 def calc_pol(path):
-   G = nx.read_edgelist(f"{path}_edgelist.csv", delimiter = ',')
+   G = nx.read_edgelist(f"{path}/edgelist.csv", delimiter = ',')
    o = {}
-   with open(f"{path}_user_scores.csv", 'r') as f:
+   with open(f"{path}/user_scores.csv", 'r') as f:
       for line in f:
          fields = line.strip().split(',')
          if fields[0] in G.nodes:
             o[fields[0]] = float(fields[1])
+   return ge(o, {}, G)
+
+
+def calc_pol2(path_edgelist, path_user_scores ):
+   df = pd.read_csv(f"{path_edgelist}", sep="\t")  # Read CSV into DataFrame
+   G = nx.from_pandas_edgelist(df, "src", "trg")
+   print(df.head())
+   print(G)
+   print(G.nodes)
+   o = {}
+#    df_user_scores = pd.read_csv(f"{path_user_scores}", sep="\t")
+   with open(f"{path_user_scores}", 'r') as f:
+      next(f)
+      for line in f:
+         fields = line.strip().split(',')
+         print(fields)
+         if int(fields[0]) in G.nodes:
+            o[int(fields[0])] = float(fields[1])
+   print(o)
+   return ge(o, {}, G)
+
+
+
+
+def calc_pol3(path_edgelist, path_user_scores, component):
+   df = pd.read_csv(f"{path_edgelist}", sep="\t")  # Read CSV into DataFrame
+   G = nx.from_pandas_edgelist(df, "src", "trg")
+   print(df.head())
+   print(G)
+   print(G.nodes)
+   o = {}
+   df_user_scores = pd.read_csv(f"{path_user_scores}", sep=",")
+   o = df_user_scores.set_index("nodeid")[f"{component}"].to_dict()
+   print(o)
    return ge(o, {}, G)
