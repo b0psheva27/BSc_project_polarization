f8beda11a5fb83ee1ff4415dc2c392a285ae8b50
b0psheva27 <peshevabozhidara@gmail.com>
2025-04-09 10:56:17 +0200

    analysis, plots and functions
    Co-Authored-By: JosefineNyeng <129183050+JosefineNyeng@users.noreply.github.com>

--- notebooks/utils/reduction_function.py
+++ notebooks/utils/reduction_function.py
@@ -42,47 +42,47 @@ def merge_datasets(data1, data2, data3):
     return merged_df 
 
 # PCA 
-def apply_pca(merged_df, colume_list): 
+def apply_pca(merged_df, colume_list, new_columns): 
     # scaler
     scaler = pre.StandardScaler()
-    merged_df[colume_list] = scaler.fit_transform(merged_df[colume_list])
+    merged_df[new_columns] = scaler.fit_transform(merged_df[colume_list])
 
     # pca 
     pca = PCA(n_components=1)
-    merged_df["pca_component"] = pca.fit_transform(merged_df[colume_list])
+    merged_df["pca_component"] = pca.fit_transform(merged_df[new_columns])
     return merged_df
 
-def apply_pca2(merged_df, colume_list, n_components): 
+def apply_pca2(merged_df, colume_list, n_components, new_columns): 
     '''Applies PCA to a df and reduces the vector to 2D'''
     # scaler
     scaler = pre.StandardScaler()
-    merged_df[colume_list] = scaler.fit_transform(merged_df[colume_list])
+    merged_df[new_columns] = scaler.fit_transform(merged_df[colume_list])
 
     # pca 
     pca = PCA(n_components=n_components)
-    merged_df["pca_component1"], merged_df["pca_component2"] = zip(*pca.fit_transform(merged_df[colume_list]))
+    merged_df["pca_component1"], merged_df["pca_component2"] = zip(*pca.fit_transform(merged_df[new_columns]))
     return merged_df
 
 # UMAP 
-def apply_umap(merged_df, column_list):
+def apply_umap(merged_df, column_list, new_columns):
     # scaler 
     scaler = pre.StandardScaler()
-    merged_df[column_list] = scaler.fit_transform(merged_df[column_list])
+    merged_df[new_columns] = scaler.fit_transform(merged_df[column_list])
 
     # umap 
     reducer_1D = umap.UMAP(n_components=1, random_state=42)
-    merged_df["umap_component"] = reducer_1D.fit_transform(merged_df[column_list])
+    merged_df["umap_component"] = reducer_1D.fit_transform(merged_df[new_columns])
     return merged_df
 
-def apply_umap2(merged_df, column_list, n_components):
+def apply_umap2(merged_df, column_list, n_components, new_columns):
     '''Applies UMAP to a df and reduces the vector to 2D'''
     # scaler 
     scaler = pre.StandardScaler()
-    merged_df[column_list] = scaler.fit_transform(merged_df[column_list])
+    merged_df[new_columns] = scaler.fit_transform(merged_df[column_list])
 
     # umap 
     reducer = umap.UMAP(n_components=n_components, random_state=42)
-    merged_df["umap_component1"], merged_df["umap_component2"] = zip(*reducer.fit_transform(merged_df[column_list]))
+    merged_df["umap_component1"], merged_df["umap_component2"] = zip(*reducer.fit_transform(merged_df[new_columns]))
     return merged_df
 
 # Plots 
@@ -125,6 +125,33 @@ def plot_c_subplot(merged_df, pc_column, color_topics):
         if i == 0:
             ax.set_ylabel('Random')  # Label only on first plot
         ax.set_title(f'Scatter plot - {color_topic}')
+ 
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_c_subplot2D(merged_df, pc_column1, pc_column2, color_topics):
+    """Function to make a subplot of scatterplots for all reduced component in 2D,
+      colored by stances on each of the 3 topics
+      
+      color_topics: list of column names (strings) where the stances are -> ["stance_abortion", "stance_marriage", "stance_political"]
+    """
+    num_plots = len(color_topics)
+    fig, axes = plt.subplots(1, num_plots, figsize=(5 * num_plots, 5), sharey=True)
+ 
+    if num_plots == 1:
+        axes = [axes]  # Ensure axes is iterable if there's only one plot
+ 
+    for i, (color_topics, ax) in enumerate(zip(color_topics, axes)):
+        num_users = merged_df.shape[0]
+ 
+        scatter = ax.scatter(merged_df[pc_column1], merged_df[pc_column2], c=merged_df[color_topics], cmap='coolwarm')
+        cbar = fig.colorbar(scatter, ax=ax, orientation='vertical', fraction=0.02, pad=0.04)
+        cbar.set_label(f'{color_topics} stance')
+ 
+        ax.set_xlabel('UMAP component 1' if pc_column1 == "umap_component1" else 'Principal component 1')
+        ax.set_ylabel('UMAP component 2' if pc_column2 == "umap_component2" else 'Principal component 2')
+        ax.set_title(f'Scatter plot - {color_topics}')
  
     plt.tight_layout()
     plt.show()
